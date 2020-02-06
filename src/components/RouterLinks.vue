998d5e85836e252d606e13ba03cfa3eb35f52173
B Ruml <b@ruml.com>
2020-02-06 11:37:30 -0500

    RouterLinks.vue v1.3

--- src/components/RouterLinks.vue
+++ src/components/RouterLinks.vue
@@ -1,34 +1,55 @@
 <template>
-    <div class="routerLinks">
-      <template v-for="(route, idx) in routesArr">
-        <!-- `<router-link>` will be rendered as an `<a>` tag by default -->
-        <router-link :to="route[0]" :key="idx">{{route[1]}}</router-link>
-        {{ idx == routesArr.length - 1 ? "" : delimiter }}
-      </template>
-    </div>
+  <div class="routerLinks">
+    <template v-for="(link, idx) in linksArr">
+      <!-- `<router-link>` will be rendered as an `<a>` tag by default -->
+      <router-link :to="link[0]" :key="idx" :style="{'color': linkColor(idx)}" ref="links">{{link[1]}}</router-link>
+      {{ idx == linksArr.length - 1 ? "" : separator }}
+    </template>
+  </div>
 </template>
 
 <script>
-  // version 1.0; 2-1-2020;
-  // div.routerLinks is required to compile;
+  // version 1.3; 2-6-2020;
   // add at bottom of router/index.js: "export {routes}";
   // for each route to be included, add the new property:
   //     linkText: "Home",
   // to omit a route from the list, don't add a linkText property
   //     or or give linkText the value undefined;
+  // div.routerLinks is required (template cannot be the root element;
   import { routes } from "@/router/index.js"
   export default {
     props: {
-      delimiter: {
+      separator: {
         type: String,
         default: " | "
+      },
+      activeLinkColor: {
+        type: String,
+        default: "#42b983"
+      },
+      inactiveLinkColor: {
+        type: String,
+        default: "#2c3e50"
       }
     },
     computed: {
-      routesArr() {
+      linksArr() {
         return routes.filter( route => route.linkText !== undefined)
           .map( (route) => [ route.path, route.linkText ])
       }
+    },
+    methods: {
+      linkColor(idx) {
+        // there will be no refs on initial render;
+        if (!this.$refs.links) return this.inactiveLinkColor
+        let el = this.$refs.links[idx].$el
+        return el.classList.contains("router-link-exact-active") ?
+          this.activeLinkColor : this.inactiveLinkColor
+      }
+    },
+    mounted() {
+      // Force the instance to render a second time to generate refs;
+      this.$forceUpdate();
     }
   }
 </script>
@@ -37,12 +58,6 @@
 .routerLinks {
   a {
     font-weight: bold;
-    color: #2c3e50;
-
-    /* class is added to router-link anchor tag when active */
-    &.router-link-exact-active {
-      color: #42b983;
-    }
   }
 }
 </style>
